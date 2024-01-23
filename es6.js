06dced7d2bbaff7aef3075f87b676e3de0d5bf66
sowmiya <sowmiyar.21ece@kongu.edu>
2024-01-23 16:02:49 +0530

    Pre requisites of React

---
+++ es6.js
@@ -0,0 +1,19 @@
+//ES6-> ECMA Script 6 standard
+/*
+//1.var,let,const
+//2.arrow function
+var Fun=()=>{
+
+    console.log("hello")
+}
+Fun()
+
+//3.destructuring
+var array=[10,20,30,40,50]
+var [a,b,c,d,e]=array
+console.log(a,b,c,d,e)
+*/
+//4.spread operator
+var array=[10,20,30,40,50]
+var arr1=[...array,60,70]
+console.log(arr1)
